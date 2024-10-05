33ac04bbd656cbf3354e882ab04e96f009c15b86
vetewe <vtiaraw2@gmail.com>
2024-10-05 10:14:38 +0700

    last lil update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.car.ui.lib)
+    implementation(libs.androidx.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
